c87b76da875622f95aab3574c52d9f34b127ee1c
Stefan Helmert <stefan.helmert@t-online.de>
2021-05-10 22:41:48 +0200

    automatic versioning

--- dnsuptools/__init__.py
+++ dnsuptools/__init__.py
@@ -1,2 +1,10 @@
-__version__ = "0.0.19"
+#!/usr/bin/env python3
+# -*- encoding: UTF8 -*-
+
+repo = git.Repo(search_parent_directories=False)
+
+version = repo.tags[-1].name if repo.head.commit == repo.tags[-1].commit else str(repo.tags[-1].name) +"~"+ repo.head.commit if len(repo.tags) > 0 else repo.head.commit
+
+__version__ =  version
+
 
