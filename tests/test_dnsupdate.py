89b9a585a4821ae4c45729d35b5e6acdf481a8af
Stefan Helmert <stefan.helmert@t-online.de>
2021-05-15 02:44:05 +0200

    tests for flatten(), extractIds() and defaultDictList()

--- tests/test_dnsupdate.py
+++ tests/test_dnsupdate.py
@@ -14,8 +14,28 @@ class TestDNSUpdate(unittest.TestCase):
         self.dnsUpdate.handler.setUserDict(inwxUserDict)
         self.dnsUpdate.handler.setPasswdDict(inwxPasswdDict)
 
-    def testDNSops(self):
+    def testFlatten(self):
+        x = [1,2,3,[4,5,[6],7],8]
+        y = dnsupdate.flatten(x)
+        self.assertEqual(y, [1,2,3,4,5,6,7,8])
 
+    def testExtractIDs(self):
+        x = [{'id': 1, 'test': 'a'},{'id': 2},[{'id': 3}, {'id': 4},[{'id': 5}],{'id': 6}]]
+        y = dnsupdate.extractIds(x)
+        self.assertEqual(y, [1,2,[3,4,[5],6]])
+
+    def testDefaultDictList(self):
+        baseDict = {'x': 1, 'y': 2}
+        dictList = [{}, {'z': 3}, {'x': 3}, {'x': 3, 'y': 4}, {'x': 5, 'y': 6, 'z': 7}]
+        y = dnsupdate.defaultDictList(baseDict, dictList)
+        self.assertEqual(y, [{'x': 1, 'y': 2}, {'x': 1, 'y': 2, 'z': 3}, {'x': 3, 'y': 2}, {'x': 3, 'y': 4}, {'x': 5, 'y': 6, 'z': 7}])
+
+
+
+
+
+
+    def testDNSops(self):
         self.dnsUpdate.delete({'name': testdomain})
         qry = self.dnsUpdate.qry({'name': testdomain})
         with self.subTest("Query result length after first delete 0"):
