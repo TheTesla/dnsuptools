71671cd1643d636bb24938cf5467637cc23b0411
Stefan Helmert <stefan.helmert@t-online.de>
2021-06-06 21:59:23 +0200

    version file path relataive to __init__.py

--- dnsuptools/__init__.py
+++ dnsuptools/__init__.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 # -*- encoding: UTF8 -*-
 
+import os
+
+here = os.path.dirname(os.path.realpath(__file__))
+versionfile = os.path.join(here, 'version')
+
+
 try:
     import git
     #from git.exc import InvalidGitRepositoryError
     repo = git.Repo(search_parent_directories=False)
     __version__ = '-'.join(repo.git.describe('--tags').split('-')[:2])
-    with open('version', 'wt') as v:
+    with open(versionfile, 'wt') as v:
         v.write(__version__)
 #except ImportError as e:
 except Exception as e:
     try:
-        with open('version', 'rt') as v:
+        with open(versionfile, 'rt') as v:
             __version__ = v.read()
     except OSError as e:
         __version__ = ''
