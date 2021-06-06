701f2b9fc605953fa3d464ef8ede2845323205b1
Stefan Helmert <stefan.helmert@t-online.de>
2021-06-06 15:09:36 +0200

    added exception for pip install - __init__.py is not in git repo

--- dnsuptools/__init__.py
+++ dnsuptools/__init__.py
@@ -3,18 +3,24 @@
 
 from simpleloggerplus import simpleloggerplus as log
 
+
+
 try:
     import git
+    from git.exc import InvalidGitRepositoryError
     repo = git.Repo(search_parent_directories=False)
     __version__ = '-'.join(repo.git.describe('--tags').split('-')[:2])
     with open('version', 'wt') as v:
         v.write(__version__)
-except ImportError as e:
+#except ImportError as e:
+except Exception as e:
     try:
         with open('version', 'rt') as v:
             __version__ = v.read()
     except OSError as e:
         log.warn("Can't open version file.")
         __version__ = ''
+#except InvalidGitRepositoryError as e:
+
 
 
